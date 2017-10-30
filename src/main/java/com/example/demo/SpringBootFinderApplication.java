af0374ef0f026ea68d17f0606d1f65a6d6c68c0b
Aymen <aymen@Aymens-MacBook-Pro.local>
2017-10-30 10:46:33 -0400

    added @ComponentScan

--- src/main/java/com/example/demo/SpringBootFinderApplication.java
+++ src/main/java/com/example/demo/SpringBootFinderApplication.java
@@ -2,11 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.example.demo.controllers.MasterController;
 
 import it.ozimov.springboot.mail.configuration.EnableEmailTools;
 
 @SpringBootApplication
 @EnableEmailTools
+@ComponentScan(basePackageClasses = MasterController.class)
 public class SpringBootFinderApplication {
 
 	public static void main(String[] args) {
